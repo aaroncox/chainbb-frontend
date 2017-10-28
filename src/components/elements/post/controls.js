f6bfe5c5dc31996def1a425527142fc57c9814a3
Aaron Cox <aaron.cox@greymass.com>
2017-10-28 19:49:56 -0400

    Ability to only return the button

--- src/components/elements/post/controls.js
+++ src/components/elements/post/controls.js
@@ -41,18 +41,25 @@ export default class PostControls extends React.Component {
         />
       )
     }
+    const button = (
+      <VoteButton
+        account={this.props.account}
+        status={this.props.status}
+        post={post}
+        loading={(processing.votes.indexOf(post._id) !== -1)}
+        error={(processing.errors[post._id] ? processing.errors[post._id] : false)}
+        onWeightChange={this.props.actions.setPreference}
+        clearVoteError={this.clearVoteError}
+        onVoteCast={this.castVote}
+        weight={this.props.preferences.votePowerPost}
+      />
+    )
+    if (this.props.onlyButton) {
+      return button
+    }
     return (
       <Segment basic clearing attached textAlign='right'>
-        <VoteButton
-          account={this.props.account}
-          status={this.props.status}
-          post={post}
-          loading={(processing.votes.indexOf(post._id) !== -1)}
-          error={(processing.errors[post._id] ? processing.errors[post._id] : false)}
-          onWeightChange={this.props.actions.setPreference}
-          clearVoteError={this.clearVoteError}
-          onVoteCast={this.castVote}
-          weight={this.props.preferences.votePowerPost} />
+        {button}
         <div className="ui mini images" style={{display: 'inline'}}>
           {voters}
         </div>
