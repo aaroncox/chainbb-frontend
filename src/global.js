a56976e2328168d5aff3278963f6dc3c54973339
Aaron Cox <aaron.cox@greymass.com>
2017-10-30 20:16:27 -0400

    Removed unused data and added new progression data

--- src/global.js
+++ src/global.js
@@ -1,20 +1,22 @@
 module.exports = {
-  REST_API: 'https://data.chainbb.com',
-  STM_Config: {
-    img_proxy_prefix: "https://steemitimages.com/",
-    ipfs_prefix: false,
-    upload_image: "https://steemitimages.com",
-  },
-  loc: {
-    en: {
-      "blogs": {
-        title: 'Blogs',
-        description: 'Personal blog posts'
-      },
-      "introductions": {
-        title: 'Introductions',
-        description: 'Introduce yourself to the Steem community!'
-      },
+    REST_API: 'https://data.chainbb.com',
+    STM_Config: {
+        img_proxy_prefix: "https://steemitimages.com/",
+        ipfs_prefix: false,
+        upload_image: "https://steemitimages.com",
+    },
+    PROGRESSION: {
+        increment: 25,
+        steps: [
+            1.375, 4.050, 7.959, 14.300,
+            23.587, 35.625, 50.225, 67.200,
+            86.362, 107.525, 130.500, 155.100,
+            178.450, 200.550, 221.400, 241.000,
+            264.225, 285.950, 306.175, 324.900,
+            344.312, 362.100, 378.262, 392.800,
+            407.650, 420.750, 432.100, 441.700,
+            451.237, 458.900, 464.687, 468.600,
+            472.075, 476.300, 480.900, 483.000
+        ]
     }
-  }
 };
