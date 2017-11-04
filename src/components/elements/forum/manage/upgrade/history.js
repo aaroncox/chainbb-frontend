96e3705570a2e13959c4758fefb1616a75c7c560
Aaron Cox <aaron.cox@greymass.com>
2017-11-04 04:21:06 -0400

    Better display on upgrade history

--- src/components/elements/forum/manage/upgrade/history.js
+++ src/components/elements/forum/manage/upgrade/history.js
@@ -12,12 +12,22 @@ export default class ForumUpgradeHistory extends React.Component {
                 return (
                     <Table.Row key={event['_id']}>
                         <Table.Cell>
-                            <a href={`https://steemd.com/b/${event['height']}#${event['_id']}`} target='_blank'>
-                                <small>{event['_id'].substring(0,8)}</small>
+                            <a href={`https://steemd.com/b/${event['height']}#${event['txid']}`} target='_blank'>
+                                <small>{event['height']}</small>
                             </a>
                         </Table.Cell>
                         <Table.Cell textAlign='right'>{event.steem_value}&nbsp;STEEM</Table.Cell>
-                        <Table.Cell><AccountLink username={event.from}/></Table.Cell>
+                        <Table.Cell><AccountLink username={event.from || event.author}/></Table.Cell>
+                        <Table.Cell>
+                            {(event.category && event.author && event.permlink)
+                                ? (
+                                    <a href={`/${event.category}/@${event.author}/${event.permlink}`}>
+                                        {event.title}
+                                    </a>
+                                )
+                                : 'Incoming Transfer'
+                            }
+                        </Table.Cell>
                         <Table.Cell textAlign='right'><TimeAgo date={`${event.timestamp}Z`} /></Table.Cell>
                     </Table.Row>
 
@@ -28,7 +38,7 @@ export default class ForumUpgradeHistory extends React.Component {
             <Table unstackable>
                 <Table.Header>
                     <Table.Row>
-                        <Table.HeaderCell collapsing>TXID</Table.HeaderCell>
+                        <Table.HeaderCell collapsing>Block</Table.HeaderCell>
                         <Table.HeaderCell collapsing textAlign='right'>Contribution</Table.HeaderCell>
                         <Table.HeaderCell>Account</Table.HeaderCell>
                         <Table.HeaderCell textAlign='right'>Date</Table.HeaderCell>
