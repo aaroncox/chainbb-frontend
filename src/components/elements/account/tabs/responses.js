1025a8acf0daa2f3aff930d6ef9524681bd9a563
Aaron Cox <aaron.cox@greymass.com>
2017-12-12 03:47:23 -0500

    Reworked responses to match replies

--- src/components/elements/account/tabs/responses.js
+++ src/components/elements/account/tabs/responses.js
@@ -1,64 +1,95 @@
 import React from 'react';
 
 import { Grid, Header, Segment } from 'semantic-ui-react'
-// import AccountAvatar from './avatar'
+import { goToTop } from 'react-scrollable-anchor'
 import AccountLink from '../link'
-import ForumPostResponse from '../../forum/post/response'
-import ForumHeader from '../../forum/header'
+import ForumPostReply from '../../forum/post/reply'
 import Paginator from '../../../global/paginator'
 
-export default class AccountPosts extends React.Component {
+export default class AccountResponses extends React.Component {
   constructor(props) {
     super(props)
+    const { post } = props;
     const { username } = props.match.params;
-    const page = 1;
-    this.state = { username, page }
+    this.state = {
+      page: 1,
+      post,
+      responses: [],
+      totalResponses: 0,
+      username
+    }
     props.actions.fetchPostResponsesByAuthor(username);
+    this.changePage = this.changePage.bind(this);
   }
   changePage = (page) => {
     this.setState({
-      page: page
+      page: page,
+      responses: []
     })
     this.props.actions.fetchPostResponsesByAuthor(this.state.username, page);
+    goToTop();
+  }
+  componentWillReceiveProps(nextProps) {
+    const { username } = this.state
+    const { post } = nextProps
+    console.log(post)
+    if(post.authors && post.authors[username] && post.authors[username].responses) {
+      const { responses, totalResponses } = post.authors[username]
+      let content = false
+      if(responses && responses.length > 0) {
+        content = responses.map((topic, idx) => <ForumPostReply topic={topic} key={idx} {... this.props} />)
+      }
+      this.setState({ content, responses, totalResponses })
+    }
   }
   render() {
-    const { username } = this.props.match.params;
-    let content = <Segment attached padded="very" loading />
-    if(this.props && this.props.post && this.props.post.authors && this.props.post.authors[username] && this.props.post.authors[username].responses) {
-      const { authors } = this.props.post;
-      const { responses, totalResponses } = authors[username];
-      content = (
-        <Segment attached>
-          <Grid>
-            <Grid.Row>
-              <Grid.Column width={8}>
-                <Header size="large">
-                  Responses to other user's posts
-                  <Header.Subheader>
-                    The most recent responses written by
-                    {' '}
-                    <AccountLink
-                      noPopup={true}
-                      username={username}
-                    />
-                  </Header.Subheader>
-                </Header>
-              </Grid.Column>
-              <Grid.Column width={8}>
-                <Paginator
-                  page={this.state.page}
-                  perPage={20}
-                  total={totalResponses}
-                  callback={this.changePage}
+    const { totalResponses, username } = this.state
+    let { content } = this.state
+    if(!content) content = <Segment attached padded="very" loading style={{margin: '2em 0'}} />
+    return (
+      <Segment attached>
+        <Grid>
+          <Grid.Row>
+            <Grid.Column width={8}>
+              <Header size="large">
+                Post Replies
+                <Header.Subheader>
+                  The most recent responses by
+                  {' '}
+                  <AccountLink
+                    noPopup={true}
+                    username={username}
                   />
-              </Grid.Column>
-            </Grid.Row>
-          </Grid>
-          <ForumHeader />
-          {responses.map((topic, idx) => <ForumPostResponse topic={topic} key={idx} />)}
-        </Segment>
-      )
-    }
-    return content
+                </Header.Subheader>
+              </Header>
+            </Grid.Column>
+            <Grid.Column width={8}>
+              <Paginator
+                page={this.state.page}
+                perPage={20}
+                total={totalResponses}
+                callback={this.changePage}
+                />
+            </Grid.Column>
+          </Grid.Row>
+        </Grid>
+        {content}
+        <Grid>
+          <Grid.Row>
+            <Grid.Column width={8}>
+
+            </Grid.Column>
+            <Grid.Column width={8}>
+              <Paginator
+                page={this.state.page}
+                perPage={20}
+                total={totalResponses}
+                callback={this.changePage}
+                />
+            </Grid.Column>
+          </Grid.Row>
+        </Grid>
+      </Segment>
+    )
   }
 }
